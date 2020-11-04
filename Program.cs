0e28fb499ebed350f0ce1c38d26e77a535bc9604
marianaSilva-26 <mariana.silvadossantos.26@gmail.com>
2020-11-04 18:25:06 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Maior2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero;
+            double maior = 0;
+
+            Console.Write("Digite o 1ºNúmero: ");
+            numero = double.Parse(Console.ReadLine());
+            if (numero > maior)
+            {
+                maior = numero;
+            }
+
+            Console.Write("Digite o 2ºNúmero: ");
+            numero = double.Parse(Console.ReadLine());
+            if (numero > maior)
+            {
+                Console.WriteLine();
+                maior = numero;
+            }
+            Console.Write("O maior número digitado é: {0}", maior);
+
+            Console.WriteLine();
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+        }
+    }
+}
